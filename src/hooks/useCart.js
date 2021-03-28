c3298d110e9c79d4c0b2b9ffc07c65fd65e68b4c
Panjamapong Sermsawatsri <panjmp@gmail.com>
2021-03-28 14:29:32 +0700

    ✨ add useCart custom hook

---
+++ src/hooks/useCart.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+export const useCart = () => {
+  const [cartItems, setCartItems] = useState([]);
+  useEffect(() => {
+    const cartItemsString = window.localStorage.getItem('cartItems');
+    const cart = cartItemsString ? JSON.parse(cartItemsString) : [];
+    setCartItems(cart);
+  }, []);
+  const addCartItem = (product, quantity) => {
+    const matchingCartItem = cartItems.find((cartItem) => cartItem.product.id === product.id);
+    if (matchingCartItem) {
+      matchingCartItem.quantity += quantity;
+    } else {
+      cartItems.push({ product, quantity });
+    }
+    setCartItems(cartItems);
+    window.localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  };
+  const removeCartItem = (productId) => {
+    const newCartItems = cartItems.filter((cartItem) => cartItem.product.id !== productId);
+    setCartItems(newCartItems);
+    window.localStorage.setItem('cartItems', JSON.stringify(newCartItems));
+  };
+  return { cartItems, addCartItem, removeCartItem };
+};
+
+export default useCart;
