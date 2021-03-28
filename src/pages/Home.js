a21ecdfbeaead846d23a6bc7c6e5660ca1ad539a
Panjamapong Sermsawatsri <panjmp@gmail.com>
2021-03-28 13:11:22 +0700

    ✨ Call API in Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,26 @@
+import { useEffect, useState } from 'react';
 import Hero from '../components/Hero';
 import ProductList from '../components/ProductList';
 
-import { products } from '../data';
-
-export const Home = () => (
-  <div>
-    <Hero />
-    <ProductList data={products} />
-  </div>
-);
+export const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://us-central1-skooldio-react-hooks.cloudfunctions.net/products')
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProducts(data);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <Hero />
+      {loading && <div>Loading</div>}
+      {!loading && <ProductList data={products} />}
+    </div>
+  );
+};
 
 export default Home;
